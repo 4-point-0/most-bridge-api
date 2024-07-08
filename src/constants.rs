b69bddbbbe378e16e1f8ad61fd79f82c0af8c837
rimatik <slaven@rimatik.hr>
2024-07-08 15:31:29 +0200

    updated constants

--- src/constants.rs
+++ src/constants.rs
@@ -1,6 +1,7 @@
-pub const MODE: &str = "dev";
-pub const SIGNER: &str = "";
-pub const GAS_OBJECT_ID: &str = "";
+pub const MODE: &str = "production";
+pub const SIGNER: &str = "0x7bdee44853cdc91bcaecb988cec5bed02febacf6988df54d74eb325d24d94e0f";
+pub const GAS_OBJECT_ID: &str =
+    "0xc887c248ae8a0a522a0941000a35e891ef036af10318a04a9b544c201c59837d";
 pub const GAS_BUDGET: &str = "1000000000";
 pub const TESTNET_URL: &str = "https://fullnode.testnet.sui.io/";
 pub const MAINNET_URL: &str = "https://fullnode.mainnet.sui.io/";
