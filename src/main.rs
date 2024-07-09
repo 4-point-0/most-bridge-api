2795d8ba685a54fead719c1a20ff46a6e4d9631d
rimatik <slaven@rimatik.hr>
2024-07-09 16:30:07 +0200

    added execute and get events apis

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 use actix_web::web::ServiceConfig;
-use actix_web::{post, web, Error as ActixError};
+use actix_web::{get, post, web, Error as ActixError};
 use base64::{self, engine::general_purpose::STANDARD, Engine};
-use constants::{GAS_BUDGET, GAS_OBJECT_ID, MAINNET_URL, MODE, SIGNER, TESTNET_URL};
+use constants::{
+    GAS_BUDGET, GAS_OBJECT_ID, MAINNET_URL, MODE, SIGNER, SUI_MODULE_ID, SUI_PACKAGE_ID,
+    TESTNET_URL,
+};
 use fastcrypto::{
     encoding::{Base64, Encoding},
     hash::HashFunction,
 };
-use models::Reply;
-use models::{PaySuiRequest, PaySuiResponse, TxDigestRequest};
+
+use models::{
+    ExecuteTxBlockRequest, ExecuteTxBlockResponse, MoveModule, QueryEventsRequest,
+    QueryEventsRpcRequest, QueryParamClassOne, QueryParamClassTwo, QueryParamElement,
+    QueryRpcResponse, Reply,
+};
+use models::{PaySuiResponse, RpcRequest, TxDigestRequest};
 use shared_crypto::intent::{Intent, IntentMessage};
 use shuttle_actix_web::ShuttleActixWeb;
 use sui_types::transaction::TransactionData;
@@ -15,7 +23,7 @@ mod constants;
 mod models;
 
 #[post("/tx-digest")]
-async fn tx_digest(dto: web::Json<TxDigestRequest>) -> Result<Reply, ActixError> {
+async fn get_tx_digest(dto: web::Json<TxDigestRequest>) -> Result<Reply, ActixError> {
     match transfer_sui(dto.clone()).await {
         Ok(tx_bytes) => {
             let decoded = Engine::decode(&STANDARD, tx_bytes.clone()).unwrap();
@@ -42,17 +50,110 @@ async fn tx_digest(dto: web::Json<TxDigestRequest>) -> Result<Reply, ActixError>
     }
 }
 
+#[post("/execute-tx-block")]
+async fn execute_tx_block(
+    dto: web::Json<ExecuteTxBlockRequest>,
+) -> Result<ExecuteTxBlockResponse, ActixError> {
+    let ExecuteTxBlockRequest {
+        signature,
+        tx_bytes,
+    } = dto.clone();
+    let model: RpcRequest = RpcRequest {
+        jsonrpc: "2.0".to_string(),
+        id: 1,
+        method: "sui_executeTransactionBlock".to_string(),
+        params: vec![signature, tx_bytes],
+    };
+
+    match reqwest::Client::new()
+        .post(match MODE {
+            "dev" => TESTNET_URL.to_string(),
+            _ => MAINNET_URL.to_string(),
+        })
+        .json(&model)
+        .send()
+        .await
+    {
+        Ok(response) => {
+            match response.json::<ExecuteTxBlockResponse>().await {
+                Ok(resp) => return Ok(resp),
+                Err(err) => panic!("Failed to execute tx block: {}", err.to_string()),
+            };
+        }
+        Err(err) => panic!("Failed to execute tx block: {}", err.to_string()),
+    };
+}
+
+#[post("/query-events")]
+async fn sui_events(
+    dto: web::Json<QueryEventsRequest>,
+) -> Result<QueryRpcResponse, Box<dyn std::error::Error>> {
+    let QueryEventsRequest { tx_digest } = dto.clone();
+
+    let mut params: Vec<QueryParamElement> = vec![];
+
+    let param_1 = QueryParamElement::QueryParamClassOne(QueryParamClassOne {
+        move_module: Some(MoveModule {
+            package: SUI_PACKAGE_ID.to_string(),
+            module: SUI_MODULE_ID.to_string(),
+        }),
+    });
+    let param_2 = QueryParamElement::Null;
+    let param_3 = QueryParamElement::Integer(18000);
+    let param_4 = QueryParamElement::Bool(false);
+
+    params.push(param_1);
+    params.push(param_2);
+    params.push(param_3);
+    params.push(param_4);
+
+    if tx_digest.len() > 0 {
+        let param = QueryParamElement::QueryParamClassTwo(QueryParamClassTwo {
+            tx_digest: Some(tx_digest),
+            event_seq: Some("0".to_string()),
+        });
+        params[1] = param;
+    }
+
+    let model: QueryEventsRpcRequest = QueryEventsRpcRequest {
+        jsonrpc: "2.0".to_string(),
+        id: 1,
+        method: "suix_queryEvents".to_string(),
+        params: params,
+    };
+
+    let response = reqwest::Client::new()
+        .post(match MODE {
+            "dev" => TESTNET_URL.to_string(),
+            _ => MAINNET_URL.to_string(),
+        })
+        .json(&model)
+        .send()
+        .await?;
+
+    let str_body = String::from_utf8(response.text().await?.into_bytes())
+        .expect("Transformed response is not UTF-8 encoded.");
+    let res: QueryRpcResponse = serde_json::from_str(&str_body)?;
+
+    return Ok(res);
+}
+
+#[get("/ping")]
+async fn ping() -> String {
+    return "Pong".to_string();
+}
+
 async fn transfer_sui(dto: TxDigestRequest) -> Result<std::string::String, reqwest::Error> {
-    let model: PaySuiRequest = PaySuiRequest {
+    let model: RpcRequest = RpcRequest {
         jsonrpc: "2.0".to_string(),
         id: 1,
         method: "unsafe_transferSui".to_string(),
         params: vec![
-            SIGNER.to_string(),        //signer
-            GAS_OBJECT_ID.to_string(), //sui_object_id
-            GAS_BUDGET.to_string(),    //gas budget
-            dto.recipient,             //recipient
-            dto.amount,                //amount
+            SIGNER.to_string(),
+            GAS_OBJECT_ID.to_string(),
+            GAS_BUDGET.to_string(),
+            dto.recipient,
+            dto.amount,
         ],
     };
 
@@ -73,7 +174,10 @@ async fn transfer_sui(dto: TxDigestRequest) -> Result<std::string::String, reqwe
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(tx_digest);
+        cfg.service(get_tx_digest)
+            .service(execute_tx_block)
+            .service(sui_events)
+            .service(ping);
     };
 
     Ok(config.into())
--- src/models.rs
+++ src/models.rs
@@ -5,7 +5,7 @@ use serde_json::Value;
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct PaySuiRequest {
+pub struct RpcRequest {
     pub jsonrpc: String,
     pub id: i64,
     pub method: String,
@@ -89,4 +89,151 @@ impl Responder for Reply {
             .content_type(ContentType::json())
             .body(body)
     }
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct ExecuteTxBlockRequest {
+    pub signature: String,
+    pub tx_bytes: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ExecuteTxBlockResponse {
+    pub jsonrpc: String,
+    pub result: ExecuteTxBlockResult,
+    pub id: i64,
+}
+
+impl Responder for ExecuteTxBlockResponse {
+    type Body = BoxBody;
+
+    fn respond_to(self, _req: &HttpRequest) -> HttpResponse<Self::Body> {
+        let body = serde_json::to_string(&self).unwrap();
+
+        HttpResponse::Ok()
+            .content_type(ContentType::json())
+            .body(body)
+    }
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct ExecuteTxBlockResult {
+    pub digest: String,
+}
+
+#[derive(Default, Serialize, Deserialize, Debug)]
+pub struct QueryEventsRpcRequest {
+    pub jsonrpc: String,
+    pub id: i64,
+    pub method: String,
+    pub params: Vec<QueryParamElement>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct QueryParamClassOne {
+    #[serde(rename = "MoveModule")]
+    pub move_module: Option<MoveModule>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct QueryParamClassTwo {
+    #[serde(rename = "txDigest")]
+    pub tx_digest: Option<String>,
+
+    #[serde(rename = "eventSeq")]
+    pub event_seq: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct MoveModule {
+    #[serde(rename = "package")]
+    pub package: String,
+
+    #[serde(rename = "module")]
+    pub module: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(untagged)]
+pub enum QueryParamElement {
+    Bool(bool),
+
+    Integer(i64),
+
+    QueryParamClassOne(QueryParamClassOne),
+    Null,
+    QueryParamClassTwo(QueryParamClassTwo),
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct QueryEventsRequest {
+    pub tx_digest: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct QueryRpcResponse {
+    pub jsonrpc: String,
+    pub result: QueryRpcResponseResult,
+    pub id: i64,
+}
+
+impl Responder for QueryRpcResponse {
+    type Body = BoxBody;
+
+    fn respond_to(self, _req: &HttpRequest) -> HttpResponse<Self::Body> {
+        let body = serde_json::to_string(&self).unwrap();
+
+        HttpResponse::Ok()
+            .content_type(ContentType::json())
+            .body(body)
+    }
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct QueryRpcResponseResult {
+    pub data: Vec<QueryRpcResponseResultData>,
+    pub next_cursor: NextCursor,
+    pub has_next_page: bool,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct QueryRpcResponseResultData {
+    pub id: Id,
+    pub package_id: String,
+    pub transaction_module: String,
+    pub sender: String,
+    #[serde(rename = "type")]
+    pub type_field: String,
+    pub parsed_json: ParsedJson,
+    pub bcs: String,
+    pub timestamp_ms: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Id {
+    pub tx_digest: String,
+    pub event_seq: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ParsedJson {
+    pub from: String,
+    #[serde(rename = "minter_address")]
+    pub minter_address: String,
+    #[serde(rename = "principal_address")]
+    pub principal_address: String,
+    pub value: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct NextCursor {
+    pub tx_digest: String,
+    pub event_seq: String,
 }
